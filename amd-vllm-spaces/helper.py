18c532f906140c7f6d34c19d7bcf3f4438cde62f
RS <richard.xgf@gmail.com>
2025-11-19 01:41:58 +0000

    amd vllm spaces

---
+++ amd-vllm-spaces/helper.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+import time
+from dotenv import load_dotenv
+load_dotenv()
+
+# Access to DO Spaces
+SPACES_URL    = os.getenv("AWS_ENDPOINT_URL", "")
+SPACES_REGION = os.getenv("AWS_REGION", "")
+SPACES_ID     = os.getenv("AWS_ACCESS_KEY_ID", "")
+SPACES_KEY    = os.getenv("AWS_SECRET_ACCESS_KEY", "")
+
+# Create the configuration file for rclone
+# https://developers.cloudflare.com/r2/examples/rclone/
+filename = os.path.expanduser("~")+"/.config/rclone/rclone.conf"
+with open(filename,'w') as f:
+    f.write("[ds]\n")
+    f.write("type = s3\n")
+    f.write("provider = DigitalOcean\n")
+    f.write("access_key_id = {}\n".format(SPACES_ID))
+    f.write("secret_access_key = {}\n".format(SPACES_KEY))
+    f.write("region = {}\n".format(SPACES_REGION))
+    f.write("endpoint = {}\n".format(SPACES_URL))
+    f.write("bucket_acl = private")
+
+# upload_local_to_cloud
+def Uploader(source, bucket, target, chunk_size_mbype="10M", concurrency="10"):    
+    cmd = f'rclone copyto {source} ds:{bucket}/{target} --s3-chunk-size={chunk_size_mbype} --transfers={concurrency} --ignore-times'
+    try:                                      
+        subprocess.run(cmd, shell=True, check=True, stderr=subprocess.PIPE)
+        print(cmd, flush=True)
+    except subprocess.CalledProcessError as e:
+        print(f"The error message: {e}", flush=True)
+
+
+
---
+++ amd-vllm-spaces/start.sh
@@ -0,0 +1,22 @@
+mkdir -p /result/$FOLDER
+
+#################### vLLM Server - Background
+
+VLLM_USE_V1=1 vllm serve $MODEL \
+ --enforce-eager \
+ --host 0.0.0.0 \
+ --port 8000 \
+ --tensor-parallel-size 8 \
+ --seed 1024 \
+ --dtype float16 \
+ --max-model-len 10000 \
+ --max-num-batched-tokens 10000 \
+ --max-num-seqs 256 \
+ --trust-remote-code \
+ --gpu-memory-utilization 0.9 > /app/vllm_server.log 2>&1 &
+
+#################### Test Client
+
+python3 test.py
+
+# python3 test.py > /app/client.log 2>&1
